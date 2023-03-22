fc6ba01995c25931dc8dc6b2c6bf48cfe33c933b
Ilya Stratovich <ilyastratovitch@yandex.by>
2023-03-22 16:48:37 +0300

    1.1

---
+++ JSON/ViewControllers/UserAction.swift
@@ -0,0 +1,14 @@
+//
+//  userAction.swift
+//  JSON
+//
+//  Created by Илья Стратович on 22.03.23.
+//
+
+import UIKit
+
+final class UserAction: UICollectionViewCell {
+    
+    @IBOutlet weak var userActionLabel: UILabel!
+    
+}
